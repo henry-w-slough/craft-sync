2c5edbbcbbdce951db68952630c6a9cb3631f1b7
Henry Slough <henry.w.slough@gmail.com>
2026-06-30 00:56:35 -0400

    Added global exception handler with basic functionality

--- client-api/main.py
+++ client-api/main.py
@@ -1,15 +1,18 @@
+#the api is truly just a test. This is not the real api, which will most likely be made in Java
+#after the backend is complete.
+
+
 import fastapi
 import uvicorn
 
 import config
+from exceptions import global_exception_handler
 
-#the api is truly just a test. This is not the real api, which will most likely be made in Java
-#after the backend is complete.
 
 app = fastapi.FastAPI()
 
 
-
+global_exception_handler.register_exception_handlers(app)
 
 
 if __name__ == "__main__":
