9416912ce20e34e302f03faba7a5da7c02a00be2
henry-w-slough <henry.w.slough@gmail.com>
2026-07-14 13:47:13 -0400

    Updating work for endpoint usage on storage backend

--- storage/controllers/world_storage_controller.py
+++ storage/controllers/world_storage_controller.py
@@ -9,5 +9,9 @@ class WorldStorageController:
     def __init__(self, app:fastapi.FastAPI, world_storage_service: WorldStorageService) -> None:
         
         self.world_storage_service = world_storage_service
+
+
+    def add_world(self) -> None:
+        return self.world_storage_service.add_world()
         
 
--- storage/repositories/world_storage_repository.py
+++ storage/repositories/world_storage_repository.py
@@ -4,5 +4,8 @@ class WorldStorageRepository:
 
 
     def __init__(self) -> None:
-        
         pass
+
+
+    def add_world(self) -> None:
+        return None
