5aff384e77cb49f8cdc2c6579e27299afc741ca8
Henry Slough <henry.w.slough@gmail.com>
2026-07-17 12:10:56 -0400

    Add endpoint now implements aioboto3 and sends a temp file to a Mini-IO instance

--- storage/config.py
+++ storage/config.py
@@ -1,5 +1,17 @@
+import os
+
 
 HOST = "localhost"
 PORT = 8040
 
+
 STORAGE_DIR = "worlds"
+
+
+#its a secret...
+CLOUD_ENDPOINT_URL = os.environ["CLOUD_ENDPOINT_URL"]
+CLOUD_ACCESS_KEY_ID = os.environ["CLOUD_ACCESS_KEY_ID"]
+CLOUD_SECRET_ACCESS_KEY = os.environ["CLOUD_SECRET_ACCESS_KEY"]
+
+
+
--- storage/repositories/world_storage_repository.py
+++ storage/repositories/world_storage_repository.py
@@ -4,10 +4,9 @@ from models.world_create_request import WorldCreateRequest
 from exceptions.world_not_found_exception import WorldNotFoundException
 
 import uuid
-import os
-import shutil
+import aioboto3
+
 import config
-import asyncio
 
 
 class WorldStorageRepository:
@@ -19,8 +18,18 @@ class WorldStorageRepository:
 
     async def add_world(self, world_create_request: WorldCreateRequest) -> WorldResponse:
         
-        os.makedirs(f"{config.STORAGE_DIR}/{world_create_request.id}")
-        
+        session = aioboto3.Session()
+
+        async with session.client( #type: ignore (aws runtime gen thing)
+            "s3",
+            endpoint_url=config.CLOUD_ENDPOINT_URL,
+            aws_access_key_id=config.CLOUD_ACCESS_KEY_ID,
+            aws_secret_access_key=config.CLOUD_SECRET_ACCESS_KEY,
+        ) as s3_client:
+            
+            await s3_client.put_object(Bucket="craftsync-worlds", Key=f"worlds/{world_create_request.id}", Body=b"")
+
+
         return WorldResponse(
             id = world_create_request.id
         )
@@ -28,12 +37,8 @@ class WorldStorageRepository:
 
     async def delete_world_by_id(self, id: uuid.UUID) -> None:
 
-        try:
-            await asyncio.to_thread(shutil.rmtree, f"{config.STORAGE_DIR}/{id}", False, None)
-        except FileNotFoundError:
-            raise WorldNotFoundException()
+        pass
     
 
 
 
-    
