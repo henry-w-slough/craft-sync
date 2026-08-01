7ed155789bb738734d4878fe56e7a036de0dfebb
henry-w-slough <henry.w.slough@gmail.com>
2026-08-01 17:42:48 -0400

    Delete world now handles nonexistent world ids

--- storage/repositories/world_storage_repository.py
+++ storage/repositories/world_storage_repository.py
@@ -5,9 +5,6 @@ from models.responses.world_update_response import WorldUpdateResponse
 from exceptions.world_not_found_exception import WorldNotFoundException
 from models.responses.world_download_response import WorldDownloadResponse
 
-from botocore.exceptions import ClientError
-from botocore.config import Config
-
 import uuid
 import aioboto3
 
@@ -45,27 +42,48 @@ class WorldStorageRepository:
             #getting object keys
             paginator = s3_client.get_paginator("list_objects_v2")
             async for page in paginator.paginate(Bucket=config.CLOUD_BUCKET_NAME, Prefix=f"worlds/{str(id)}"):
-                for object in page["Contents"]: 
-                    #setting key pair for s3 translation
-                    object_keys.append({"Key": object["Key"]})
+
+                try:
+                    for object in page["Contents"]: 
+                        #setting key pair for s3 translation
+                        object_keys.append({"Key": object["Key"]})
+                #nonreal id
+                except KeyError:
+                    raise WorldNotFoundException
 
             await s3_client.delete_objects(
                 Bucket=config.CLOUD_BUCKET_NAME,
                 Delete={"Objects": object_keys}
             )
-            
+
 
     async def update_world_by_id(self, id: uuid.UUID, world_update_request: WorldUpdateRequest) -> WorldUpdateResponse:
 
-        path_urls = {}
+        #this tracks all unchanged files, as told by the client request
+        keys_to_delete: list[dict[str, str]] = []
+        presigned_urls = {}
 
         async with self.session.client("s3", endpoint_url=config.CLOUD_ENDPOINT_URL, aws_access_key_id=config.CLOUD_ACCESS_KEY_ID, aws_secret_access_key=config.CLOUD_SECRET_ACCESS_KEY) as s3_client: #type: ignore
-            
-            for path in world_update_request.relative_paths:
-                path_urls[path] = await s3_client.generate_presigned_url("put_object", Params={"Bucket": config.CLOUD_BUCKET_NAME, "Key": f"worlds/{id}/{path}"}, ExpiresIn=600)
+
+            #getting object keys
+            paginator = s3_client.get_paginator("list_objects_v2")
+            async for page in paginator.paginate(Bucket=config.CLOUD_BUCKET_NAME, Prefix=f"worlds/{str(id)}"):
+                for object in page["Contents"]: 
+
+                    if object["Key"] in world_update_request.relative_paths:
+                        #setting key pair for s3 translation
+                        keys_to_delete.append({"Key": object["Key"]})
+                        continue
+                    
+                    presigned_urls[object["Key"].removeprefix(f"worlds/{id}/")] = await s3_client.generate_presigned_url("get_object", Params={"Bucket": config.CLOUD_BUCKET_NAME, "Key": object["Key"]})
+
+            await s3_client.delete_objects(
+                Bucket=config.CLOUD_BUCKET_NAME,
+                Delete={"Objects": keys_to_delete}
+            )
 
         return WorldUpdateResponse(
-            path_presigned_urls=path_urls
+            path_presigned_urls=presigned_urls
         )
 
 
